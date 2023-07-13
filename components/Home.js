fb35e1c0134e4ac6cdbc1771c5c0430571759d32
bertrandsimon <trandber@hotmail.com>
2023-07-13 14:01:07 +0200

    Boilerplate

--- components/Home.js
+++ components/Home.js
@@ -22,6 +22,7 @@ function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h2>TEST DE COMMIT</h2>
       </main>
     </div>
   );
