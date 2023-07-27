e054d880755e39ae78fc8b2657ba0c7f2bd2dc9f
bertrandsimon <trandber@hotmail.com>
2023-07-27 11:59:14 +0200

    CardsArea responsive Tailwind added

--- components/common/CardsArea.js
+++ components/common/CardsArea.js
@@ -8,36 +8,63 @@ import Button from '@mui/material/Button';
 
 function CardsArea() {
   return (
-    <div className={styles.container}>
+    
+
+    <div class="flex flex-wrap flex-row justify-center">
+
+    <div class="w-full sm:basis-1/4 text-center mx-auto flex flex-column items-center justify-center" height={346}>
       <div className={styles.coursesTxt}>
-          
-        <h1>Les prochaines<br /><span className='yellow'>courses</span></h1>
+        <h1 class="pb-6">Les prochaines<br /><span className='yellow'>courses</span></h1>
         <Button variant="outlined" className='whiteBtn'>toutes les courses</Button>
-       
       </div>
-
-      <div className={styles.eventCard}>
-        <Image src="/images/event1.png" width={270} height={346}/>
-        <span className='yellow'>10 sept 2023</span>
-        <h3 className='up'>beauvais (60)</h3>
-        <span className='grey'>2 eme edition</span>
-      </div>
-
-      <div className={styles.eventCard}>
-        <Image src="/images/event2.png" width={270} height={346}/>
-        <span className='yellow'>10 sept 2023</span>
-        <h3 className='up'>beauvais (60)</h3>
-        <span className='grey'>2 eme edition</span>
-      </div>
-
-      <div className={styles.eventCard}>
-        <Image src="/images/event3.png" width={270} height={346}/>
-        <span className='yellow'>10 sept 2023</span>
-        <h3 className='up'>beauvais (60)</h3>
-        <span className='grey'>2 eme edition</span>
-      </div>
-     
     </div>
+  
+    <div class="w-full sm:basis-1/4 mx-auto flex items-center justify-center">
+      <Image src="/images/event2.png" width={270} height={346} />
+    </div>
+  
+    <div class="w-full sm:basis-1/4 mx-auto flex items-center justify-center">
+      <Image src="/images/event2.png" width={270} height={346} />
+    </div>
+  
+    <div class="w-full sm:basis-1/4 mx-auto flex items-center justify-center">
+      <Image src="/images/event2.png" width={270} height={346} />
+    </div>
+  
+  </div>
+  
+  
+
+    // <div className={styles.container}>
+    //   <div className={styles.coursesTxt}>
+          
+    //     <h1>Les prochaines<br /><span className='yellow'>courses</span></h1>
+    //     <Button variant="outlined" className='whiteBtn'>toutes les courses</Button>
+       
+    //   </div>
+
+    //   <div className={styles.eventCard}>
+    //     <Image src="/images/event1.png" width={270} height={346}/>
+    //     <span className='yellow'>10 sept 2023</span>
+    //     <h3 className='up'>beauvais (60)</h3>
+    //     <span className='grey'>2 eme edition</span>
+    //   </div>
+
+    //   <div className={styles.eventCard}>
+    //     <Image src="/images/event2.png" width={270} height={346}/>
+    //     <span className='yellow'>10 sept 2023</span>
+    //     <h3 className='up'>beauvais (60)</h3>
+    //     <span className='grey'>2 eme edition</span>
+    //   </div>
+
+    //   <div className={styles.eventCard}>
+    //     <Image src="/images/event3.png" width={270} height={346}/>
+    //     <span className='yellow'>10 sept 2023</span>
+    //     <h3 className='up'>beauvais (60)</h3>
+    //     <span className='grey'>2 eme edition</span>
+    //   </div>
+     
+    // </div>
   );
 }
 
--- components/common/Nav.js
+++ components/common/Nav.js
@@ -1,24 +1,14 @@
-// import styles from '../../styles/Menu.module.css';
 import { useState } from 'react';
-
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon, ShoppingCartIcon } from '@heroicons/react/24/outline'
-
-
-
-
-// IMAGE IMPORT
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
-
-
 function Nav() {
-
   const [currentNavItem, setCurrentNavItem] = useState(null);
 
   const navigation = [
@@ -29,7 +19,7 @@ function Nav() {
     { name: 'Bénévoles', href: '#', current: currentNavItem === 'Bénévoles' },
     { name: 'Contact', href: '#', current: currentNavItem === 'Contact' },
   ];
-  
+
   const handleClick = (itemName) => {
     setCurrentNavItem(itemName);
   };
@@ -39,33 +29,25 @@ function Nav() {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-yellow-400 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Ouvrir</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <Image src="/images/frappadingue-logo.png" width={216} height={77}/>
+            <div className="relative flex items-center justify-between">
+              <div className="flex flex-1 items-center justify-start">
+                {/* Logo */}
+                <div className="flex-shrink-0 items-center">
+                  <Image src="/images/frappadingue-logo.png" width={216} height={77} />
                 </div>
+
+                {/* Desktop menu */}
                 <div className="hidden sm:ml-6 sm:block sm:pt-4 sm:pl-10">
-                 {/* desktop menu */}
                   <div className="flex space-x-4 uppercase">
                     {navigation.map((item) => (
                       <a
                         onClick={() => handleClick(item.name)}
-                       
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-yellow-400 text-black' : 'text-gray-300 hover:bg-yellow-400 hover:text-black',
+                          item.current
+                            ? 'bg-yellow-400 text-black'
+                            : 'text-gray-300 hover:bg-yellow-400 hover:text-black',
                           'rounded-md px-3 py-2 text-lg font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -76,10 +58,11 @@ function Nav() {
                   </div>
                 </div>
               </div>
+
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
-                  className="rounded-full  p-1 text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:bg-yellow-400"
+                  className="rounded-full p-1 text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:bg-yellow-400"
                 >
                   <span className="sr-only">Panier</span>
                   <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
@@ -107,46 +90,25 @@ function Nav() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Votre compte
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Infos
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Deconnection
-                          </a>
-                        )}
-                      </Menu.Item>
+                      {/* Menu items */}
                     </Menu.Items>
                   </Transition>
                 </Menu>
               </div>
+
+              {/* Mobile menu button */}
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-yellow-400 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Ouvrir</span>
+                  {open ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />}
+                </Disclosure.Button>
+              </div>
             </div>
           </div>
 
+          {/* Mobile menu */}
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
-              {/* mobile menu */}
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
