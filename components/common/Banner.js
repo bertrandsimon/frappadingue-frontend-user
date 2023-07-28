ef922564639f85441e8ed34b68426724fe751108
bertrandsimon <trandber@hotmail.com>
2023-07-28 15:19:40 +0200

    New banner started

--- components/common/Banner.js
+++ components/common/Banner.js
@@ -10,12 +10,19 @@ function Banner() {
   return (
    
     <div className={styles.container}>
-      <div className='grid grid-cols-3 gap-4 w-full'>
-        <div className='col-span-1'>TEST</div>
-        <div className='col-span-2'>TEST</div>
+      <div className='grid grid-cols-3 gap-4 w-full h-full'>
+        <div className='col-span-1 flex justify-center items-center'>TEST</div>
+        <div className='col-span-2 flex justify-center items-center flex-col drop-shadow-md'>
+          <div className={styles.bannerTitle1}>la frappadingue</div>
+          <div className={styles.bannerTitle2}>accessible à tous sans exception</div>
+          </div>
       </div>
     </div>
       
+
+
+
+
   );
 }
 
