a072981dbe923aafefabbc14a6b80fe8a3d6a2e5
bertrandsimon <trandber@hotmail.com>
2023-07-29 09:12:03 +0200

    Mini slider functional logic implemented

--- components/common/MiniSlider.js
+++ components/common/MiniSlider.js
@@ -1,27 +1,48 @@
 import styles from '../../styles/MiniSlider.module.css';
-
 import { useState } from 'react';
-// IMAGE IMPORT
 import Image from 'next/image';
 
- // <Image src="/images/slide1.png" width={1335} height={428}/>
-
+import { ChevronRightIcon } from '@heroicons/react/24/solid';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 
 function MiniSlider() {
-
   const images = [
-    { id: 1, src: '/images/1.png', alt: 'slide1', current: true },
-    { id: 2, src: '/images/2.png', alt: 'slide1', current: true },
-    { id: 3, src: '/images/3.png', alt: 'slide1', current: true },
-
+    { id: 0, src: '/images/1.png', alt: 'slide1', current: false },
+    { id: 1, src: '/images/2.png', alt: 'slide1', current: true },
+    { id: 2, src: '/images/3.png', alt: 'slide1', current: false },
   ];
 
+  // Set the initial state to the index of the image with current: true
+  const [currentImgIndex, setCurrentImgIndex] = useState(
+    images.findIndex((image) => image.current === true)
+  );
+
+  const handlePrev = () => {
+    setCurrentImgIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+  
+  const handleNext = () => {
+    setCurrentImgIndex((prevIndex) => (prevIndex + 1) % images.length);
+
+    // Update the current property of images to indicate the current image
+    const updatedImages = images.map((image, index) => ({
+      ...image,
+      current: index === currentImgIndex,
+    }));
+    // Update the images array with the updated current property
+    // You can also use a state variable for images if you want to re-render the component with the updated images.
+  };
+
+  const currentImage = images[currentImgIndex];
+
   return (
-   
     <div className={styles.container}>
-      <Image src="/images/2.png" width={488} height={404}/>
+      <div onClick={handlePrev}><ChevronLeftIcon className="w-6 h-6 text-black" /></div>
+      <Image src={currentImage.src} width={488} height={404} />
+      <div onClick={handleNext}><ChevronRightIcon className="w-6 h-6 text-black" /></div>
     </div>
-      
   );
 }
 
