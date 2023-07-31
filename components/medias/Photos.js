ecc5b004bb7ed4326f53fec611335e7ab49e1eb5
bertrandsimon <trandber@hotmail.com>
2023-07-31 15:10:29 +0200

    Photos page created + images resized

--- components/medias/Photos.js
+++ components/medias/Photos.js
@@ -1,20 +1,20 @@
 import 'lightbox.js-react/dist/index.css';
 import { SlideshowLightbox } from 'lightbox.js-react';
+import { itemsHorizontal, itemsVertical} from './PhotosData';
 
 function Photos() {
+
+  const photosHorizontal = itemsHorizontal.map( (item) => (<img className='w-full rounded' src={item.src} alt={item.alt} /> ))
+  const photosVertical = itemsVertical.map( (item) => (<img className='w-full rounded' src={item.src} alt={item.alt} /> ))
+
   return (
-    <div>
-      <h1>PHOTOS</h1>
-      <SlideshowLightbox className='container grid grid-cols-3 gap-2 mx-auto' showThumbnails={true}>
-        <img className='w-full rounded' src='https://images.pexels.com/photos/580151/pexels-photo-580151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 1" />
-        <img className='w-full rounded' src='https://images.pexels.com/photos/13996896/pexels-photo-13996896.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 2" />  
-        <img className='w-full rounded' src='https://images.pexels.com/photos/13208323/pexels-photo-13208323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 3" />
-        <img className='w-full rounded' src='https://images.pexels.com/photos/580151/pexels-photo-580151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 1" />
-        <img className='w-full rounded' src='https://images.pexels.com/photos/13996896/pexels-photo-13996896.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 2" />  
-        <img className='w-full rounded' src='https://images.pexels.com/photos/13208323/pexels-photo-13208323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 3" />
-        <img className='w-full rounded' src='https://images.pexels.com/photos/580151/pexels-photo-580151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 1" />
-        <img className='w-full rounded' src='https://images.pexels.com/photos/13996896/pexels-photo-13996896.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 2" />  
-        <img className='w-full rounded' src='https://images.pexels.com/photos/13208323/pexels-photo-13208323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="Image 3" />
+    <div className='p-10'>
+      
+      <SlideshowLightbox className='container grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto p-10' showThumbnails={true}>
+
+        {photosHorizontal}
+
+      
       </SlideshowLightbox> 
     </div>
   );
