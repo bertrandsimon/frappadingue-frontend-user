d95c3e01bf97871ca01ec024dfa473f704f25641
bertrandsimon <trandber@hotmail.com>
2023-08-14 14:36:13 +0200

    Product options + stripe payment

--- components/shop/Product.js
+++ components/shop/Product.js
@@ -1,6 +1,7 @@
 import { useShoppingCart } from "use-shopping-cart";
 import { useState } from "react";
-// import { formatCurrencyString } from "use-shopping-cart";
+import Button from '@mui/material/Button';
+
 
 export default function Product({ product }) {
 
@@ -25,10 +26,10 @@ export default function Product({ product }) {
   };
 
   return (
-    <article className="flex flex-col gap-3 bg-white p-8 rounded-xl shadow-md text-center mb-6">
-      <div className="text-8xl cursor-default">{emoji}</div>
+    <article className="flex flex-col gap-1  p-8 rounded-xl shadow-md text-center mb-6 bg-zinc-900">
+      <div className="text-4xl cursor-default">{emoji}</div>
       <div className="text-lg">{name}</div>
-      <div className="text-2xl font-semibold mt-auto">
+      <div className="text-2xl text-normal mt-auto">
 
         {price} <span> €</span>
 
@@ -36,21 +37,22 @@ export default function Product({ product }) {
       <div className="flex justify-around items-center mt-4 mb-2 ">
         <button
           onClick={decreaseQuantity}
-          className="hover:text-emerald-500 hover:bg-emerald-50 w-8 h-8 rounded-full transition-colors duration-500"
+          className="hover:text-black hover:bg-emerald-50 w-8 h-8 rounded-full transition-colors duration-500"
         >
           -
         </button>
-        <span className="w-10 text-center rounded-md mx-3">{quantity}</span>
+        <span className="w-8 text-center rounded-md mx-3 text-yellow-400">{quantity}</span>
         <button
           onClick={increaseQuantity}
-          className="hover:text-emerald-500 hover:bg-emerald-50 w-8 h-8 rounded-full transition-colors duration-500"
+          className="hover:text-black hover:bg-emerald-50 w-8 h-8 rounded-full transition-colors duration-500"
         >
           +
         </button>
       </div>
-      <button onClick={ () => addToCart()} className="bg-emerald-50 hover:bg-emerald-500 hover:text-white transition-colors duration-500 text-emerald-500 rounded-md px-5 py-2">
-        Ajouter au panier
-      </button>
+     
+      <Button onClick={ () => addToCart()} variant="outlined" className='whiteBtn'>
+          Ajouter au panier
+      </Button>
     </article>
   );
 }
--- components/shop/ProductOptions.js
+++ components/shop/ProductOptions.js
@@ -8,14 +8,22 @@ function ProductOptions() {
   console.log(products)
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center mx-auto gap-4 place-center flex-wrap w-100 md:max-w-[1400px]">
-      <h1>Product options</h1>
 
-      {products.map((product) => (
-        <Product product={product} key={product.id} />
-      ))}
+    <div>
+      
+      <h1>Options</h1>
+     
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center mx-auto gap-4 place-center flex-wrap w-100 md:max-w-[1400px]">
+    
+        {products.map((product) => (
+          <Product product={product} key={product.id} />
+        ))}
 
-    </div>
+      </div>
+
+    </div> 
+
+
   );
 }
 
