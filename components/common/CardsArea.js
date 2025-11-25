2f2aabd90806d3e3f48599648c65f255ff8574b0
logeen <trandber@hotmail.com>
2025-11-25 16:13:25 +0100

    Modifications

--- components/common/CardsArea.js
+++ components/common/CardsArea.js
@@ -15,15 +15,24 @@ function CardsArea( {eventsCount} ) {
     <div className="grid grid-cols-1 sm:grid-cols-4 justify-center pt-20 mt-20 sm:-mt-16 sm:pt-0 pr-8 pl-8">
 
     <div className="col-span-1 w-full sm:basis-1/4 text-center mx-auto flex flex-column items-center justify-center pb-6" height={346}>
-      <div className='pt-10'>
+    
+    
+    
+    <div className='flex flex-col justify-center items-center gap-4'>
+    <Image src="/images/events/girls.png" width={211} height={211} alt="courses" />
+
+
+    <div className='pt-10'>
         <p className="pb-2 text-2xl">Les prochaines</p>
         <p className='text-[#ffe500] text-2xl pb-8'>courses</p>
           <Button variant="outlined" className='whiteBtn'>
+            
             <Link href="/CoursesPage">toutes les courses</Link>
           </Button>
       </div>
     </div>
-    <div className='col-span-3'><EventsCardsGrid eventsCount={eventsCount}></EventsCardsGrid></div>
+    </div>
+    <div className='col-span-3 pb-10'><EventsCardsGrid eventsCount={eventsCount}></EventsCardsGrid></div>
   
   </div>
   
